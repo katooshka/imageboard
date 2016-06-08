74cd12b3de2d0a0f85160b5b6bb43f47b4e45e1e
katooshka <ek.zabara@yandex.ru>
2016-06-08 23:38:30 +0100

    Fix sql script, resolves bug #7

--- src/main/resources/table_definitions.sql
+++ src/main/resources/table_definitions.sql
@@ -7,17 +7,6 @@ CREATE TABLE boards (
   UNIQUE KEY address (address)
 );
 
-CREATE TABLE posts (
-  id        INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  thread_id INT(11) UNSIGNED NOT NULL,
-  post_time VARCHAR(100)     NOT NULL,
-  author    VARCHAR(100)     NOT NULL,
-  message   VARCHAR(1000)    NOT NULL,
-  PRIMARY KEY (id),
-  KEY post_to_thread (thread_id),
-  CONSTRAINT post_to_thread FOREIGN KEY (thread_id) REFERENCES threads (id)
-);
-
 CREATE TABLE threads (
   id       INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   board_id INT(11) UNSIGNED NOT NULL,
@@ -25,3 +14,16 @@ CREATE TABLE threads (
   KEY threads_to_boards (board_id),
   CONSTRAINT threads_to_boards FOREIGN KEY (board_id) REFERENCES boards (id)
 );
+
+CREATE TABLE posts (
+id        INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+thread_id INT(11) UNSIGNED NOT NULL,
+post_time VARCHAR(100)     NOT NULL,
+author    VARCHAR(100)     NOT NULL,
+message   VARCHAR(1000)    NOT NULL,
+PRIMARY KEY (id),
+KEY post_to_thread (thread_id),
+CONSTRAINT post_to_thread FOREIGN KEY (thread_id) REFERENCES threads (id)
+);
+
+
